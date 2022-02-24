ea4dd75aa10bfaf265406759afcb876758ebd31d
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-02-24 09:29:13 -0400

    refactor: add user identification modules

--- my_comics/lib/app/app_module.dart
+++ my_comics/lib/app/app_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:my_comics/app/modules/splash/splash_module.dart';
+import 'package:my_comics/app/modules/user_identification/userIdentification_module.dart';
 
 import 'modules/home/home_module.dart';
 
@@ -10,6 +11,7 @@ class AppModule extends Module {
   @override
   final List<ModularRoute> routes = [
     ModuleRoute(Modular.initialRoute, module: SplashModule()),
-    ModuleRoute('/home/', module: HomeModule())
+    ModuleRoute('/home/', module: HomeModule()),
+    ModuleRoute('/user_identifcation/', module: UserIdentificationModule())
   ];
 }
