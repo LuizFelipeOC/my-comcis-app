79a26025cc7f2fc5bd2207db53b6e74e8a6a805b
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-02-25 09:36:37 -0400

    fix: apply corrections in map objects

--- my_comics/lib/app/modules/home/models/comics_models.dart
+++ my_comics/lib/app/modules/home/models/comics_models.dart
@@ -1,93 +1,179 @@
-class ComicsModels {
+class Comics {
+  String? copyright;
   Data? data;
 
-  ComicsModels(
+  Comics({
+    this.copyright,
     this.data,
-  );
+  });
 
-  ComicsModels.fromJson(Map<String, dynamic> json) {
+  Comics.fromJson(Map<String, dynamic> json) {
+    copyright = json['copyright'];
     data = json['data'] != null ? Data.fromJson(json['data']) : null;
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-
+    data['copyright'] = copyright;
     if (this.data != null) {
-      data['data'] = this.data?.toJson();
+      data['data'] = this.data!.toJson();
     }
     return data;
   }
 }
 
 class Data {
-  int? offset;
-  int? limit;
-  int? total;
-  int? count;
-  List<Comic>? comics;
+  List<Results>? results;
 
-  Data({this.offset, this.limit, this.total, this.count, this.comics});
+  Data({this.results});
 
   Data.fromJson(Map<String, dynamic> json) {
-    offset = json['offset'];
-    limit = json['limit'];
-    total = json['total'];
-    count = json['count'];
     if (json['results'] != null) {
-      comics = <Comic>[];
+      results = <Results>[];
       json['results'].forEach((v) {
-        comics?.add(Comic.fromJson(v));
+        results!.add(Results.fromJson(v));
       });
     }
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data['offset'] = offset;
-    data['limit'] = limit;
-    data['total'] = total;
-    data['count'] = count;
-    data['results'] = comics?.map((v) => v.toJson()).toList();
+    if (results != null) {
+      data['results'] = results!.map((v) => v.toJson()).toList();
+    }
     return data;
   }
 }
 
-class Comic {
+class Results {
   int? id;
+  int? digitalId;
   String? title;
-  String? variantDescription;
+  int? issueNumber;
   String? description;
+  String? modified;
+  String? upc;
+  String? format;
   int? pageCount;
+  List<TextObjects>? textObjects;
+  String? resourceURI;
+  List<Prices>? prices;
   Thumbnail? thumbnail;
+  int? returned;
 
-  Comic({
+  Results({
     this.id,
+    this.digitalId,
     this.title,
-    this.variantDescription,
+    this.issueNumber,
     this.description,
+    this.modified,
+    this.upc,
+    this.format,
     this.pageCount,
+    this.textObjects,
+    this.resourceURI,
+    this.prices,
     this.thumbnail,
+    this.returned,
   });
 
-  Comic.fromJson(Map<String, dynamic> json) {
+  Results.fromJson(Map<String, dynamic> json) {
     id = json['id'];
+    digitalId = json['digitalId'];
     title = json['title'];
-    variantDescription = json['variantDescription'];
+    issueNumber = json['issueNumber'];
     description = json['description'];
+    modified = json['modified'];
+    upc = json['upc'];
+    format = json['format'];
     pageCount = json['pageCount'];
-
+    if (json['textObjects'] != null) {
+      textObjects = <TextObjects>[];
+      json['textObjects'].forEach((v) {
+        textObjects!.add(TextObjects.fromJson(v));
+      });
+    }
+    resourceURI = json['resourceURI'];
+    if (json['prices'] != null) {
+      prices = <Prices>[];
+      json['prices'].forEach((v) {
+        prices!.add(Prices.fromJson(v));
+      });
+    }
     thumbnail = json['thumbnail'] != null
         ? Thumbnail.fromJson(json['thumbnail'])
         : null;
+    returned = json['returned'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id;
+    data['digitalId'] = digitalId;
     data['title'] = title;
-    data['variantDescription'] = variantDescription;
+    data['issueNumber'] = issueNumber;
     data['description'] = description;
+    data['modified'] = modified;
+    data['upc'] = upc;
+    data['format'] = format;
     data['pageCount'] = pageCount;
+    if (textObjects != null) {
+      data['textObjects'] = textObjects!.map((v) => v.toJson()).toList();
+    }
+    data['resourceURI'] = resourceURI;
+    if (prices != null) {
+      data['prices'] = prices!.map((v) => v.toJson()).toList();
+    }
+    if (thumbnail != null) {
+      data['thumbnail'] = thumbnail!.toJson();
+    }
+    data['returned'] = returned;
+    return data;
+  }
+}
+
+class TextObjects {
+  String? type;
+  String? language;
+  String? text;
+
+  TextObjects({
+    this.type,
+    this.language,
+    this.text,
+  });
+
+  TextObjects.fromJson(Map<String, dynamic> json) {
+    type = json['type'];
+    language = json['language'];
+    text = json['text'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['type'] = type;
+    data['language'] = language;
+    data['text'] = text;
+    return data;
+  }
+}
+
+class Prices {
+  String? type;
+  dynamic price;
+
+  Prices({this.type, this.price});
+
+  Prices.fromJson(Map<String, dynamic> json) {
+    type = json['type'];
+    price = json['price'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['type'] = type;
+    data['price'] = price;
     return data;
   }
 }
@@ -96,26 +182,12 @@ class Thumbnail {
   String? path;
   String? extension;
 
-  Thumbnail({this.path, this.extension});
+  Thumbnail({
+    this.path,
+    this.extension,
+  });
 
   Thumbnail.fromJson(Map<String, dynamic> json) {
-    path = json['path'];
-    extension = json['extension'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['path'] = path;
-    data['extension'] = extension;
-    return data;
-  }
-}
-
-class Images {
-  String? path;
-  String? extension;
-  Images({this.path, this.extension});
-  Images.fromJson(Map<String, dynamic> json) {
     path = json['path'];
     extension = json['extension'];
   }
