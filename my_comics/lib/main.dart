0b859605acfe9acc343442dd08375ec3cc559be0
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-03-01 16:05:11 -0400

    refactor: initializy database

--- my_comics/lib/main.dart
+++ my_comics/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:my_comics/app/database/database.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => runApp(
-      ModularApp(
-        module: AppModule(),
-        child: const AppWidget(),
-      ),
-    );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DatabaseHandler().initializeDB();
+
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    ),
+  );
+}
