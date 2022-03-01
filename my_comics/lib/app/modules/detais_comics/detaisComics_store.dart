94aad8b2fab7dd1c9d53dca45ef40c006d22ed1b
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-03-01 16:04:33 -0400

    refactor: add  comics database and return home screen

--- my_comics/lib/app/modules/detais_comics/detaisComics_store.dart
+++ my_comics/lib/app/modules/detais_comics/detaisComics_store.dart
@@ -1,15 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
+import 'package:my_comics/app/database/database.dart';
+import 'package:my_comics/app/modules/home/models/comics_models.dart';
 
 part 'detaisComics_store.g.dart';
 
 class DetaisComicsStore = _DetaisComicsStoreBase with _$DetaisComicsStore;
-abstract class _DetaisComicsStoreBase with Store {
 
+abstract class _DetaisComicsStoreBase with Store {
   @observable
   int value = 0;
 
   @action
-  void increment() {
-    value++;
-  } 
+  addItensInCart(Results? results, image, prices) async {
+    final database = await DatabaseHandler().initializeDB();
+    final batch = database.batch();
+
+    batch.insert(
+      'CARTS',
+      {
+        'title': results?.title,
+        'image': image,
+        'price': await prices,
+        'idcomics': results?.id,
+      },
+    );
+
+    await batch.commit(noResult: true);
+
+    Modular.to.pop();
+  }
 }
